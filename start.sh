a5f3aebf5d413d621241f4c4f1a8369e27af165c
Josh Barber <j.barber501@gmail.com>
2017-11-18 21:29:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-git clone https://github.com/joshua-barber/flask-react.git
-cd flask-react
-
-pip install -r requirements.txt
+conda install --yes --file requirements.txt
 npm install
-
-npm run watch 
 
 export FLASK_APP=server.py
 
